2c0ab289570c1f547497da78ddc0d41e5e751f7e
N <cubingniles@gmail.com>
2021-05-26 23:33:48 +0530

    Add highscores display functionality and modified end.js file

--- end.js
+++ end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
-const mostRecentScore = document.querySelector('#mostRecentScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
@@ -10,7 +10,7 @@ const MAX_HIGH_SCORES = 5;
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.nodeValue;
+    saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = e => {
