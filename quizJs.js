efb119678a42d0849c22ab9b822e0ca963b1a08c
N <cubingniles@gmail.com>
2021-05-29 00:30:30 +0530

    Add new styles and responsiveness

--- quizJs.js
+++ quizJs.js
@@ -1,13 +1,34 @@
 const flipCard = document.querySelector('.allcards');
-
+const navbar = document.querySelector('#navbar');
+const footer = document.querySelector('.footer');
 flipCard.addEventListener('mouseover', () => {
   bgChange('black');
+  navbar.classList.add('hoverNav');
+  footer.classList.add('hoverFoot');
+  // navbarBgChange('#ccff00');
 })
 
 flipCard.addEventListener('mouseout', () => {
   bgChange('#ccff00');
+  navbar.classList.remove('hoverNav');
+  footer.classList.remove('hoverFoot');
+  // navbarBgChange('#000000');
 })
 
 function bgChange(bg) {
   document.body.style.background = bg;
 }
+
+function navbarBgChange(bg) {
+  const navbar = document.querySelector('.nav-container');
+  navbar.style.background = bg;
+}
+
+// Navbar
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
